1afe1b43e3e4d8372b0e79c6444ab94be3147ce8
ilja-khabarov <ilja.khabarov@gmail.com>
2016-05-26 16:56:26 +0600

    Disposed.

--- Khabarov.Nsudotnet.Enigma/Enigma/Encryptor.cs
+++ Khabarov.Nsudotnet.Enigma/Enigma/Encryptor.cs
@@ -17,36 +17,38 @@ namespace Enigma
         //static string filename = "C:/Users/ilja/Documents/Visual Studio 2015/Projects/Enigma/ToEncrypt.txt";
         //static string encryptedFile = "C:/Users/ilja/Documents/Visual Studio 2015/Projects/Enigma/encrypted";
         //private static string outfile = "C:/Users/ilja/Documents/Visual Studio 2015/Projects/Enigma/out.txt";
+        static SymmetricAlgorithm _symmetricAlgorithm;
         public static void Encrypt(string[] args) 
         {
             try
             {
-                SymmetricAlgorithm symmetricAlgorithm;
+
                 switch (args[3])
                 {
                     case "rc2":
-                        symmetricAlgorithm = RC2.Create();
+                        _symmetricAlgorithm = RC2.Create();
                         break;
                     case "aes":
-                        symmetricAlgorithm = Aes.Create();
+                        _symmetricAlgorithm = Aes.Create();
                         break;
                     case "des":
-                        symmetricAlgorithm = DES.Create();
+                        _symmetricAlgorithm = DES.Create();
                         break;
                     case "rij":
-                        symmetricAlgorithm = Rijndael.Create();
+                        _symmetricAlgorithm = Rijndael.Create();
                         break;
                     default:
-                        symmetricAlgorithm = Aes.Create();
+                        _symmetricAlgorithm = Aes.Create();
                         break;
                 }
                 if (args[0] == "encrypt")
                 {
                     string filename = args[1];
                     string encryptedFile = args[3];
-                    EncryptStringToBytes(filename, encryptedFile, symmetricAlgorithm.Key, symmetricAlgorithm.IV, symmetricAlgorithm);
-                    string key = Convert.ToBase64String(symmetricAlgorithm.Key);
-                    string IV = Convert.ToBase64String(symmetricAlgorithm.IV);
+                    EncryptStringToBytes(filename, encryptedFile, _symmetricAlgorithm.Key, _symmetricAlgorithm.IV,
+                        _symmetricAlgorithm);
+                    string key = Convert.ToBase64String(_symmetricAlgorithm.Key);
+                    string IV = Convert.ToBase64String(_symmetricAlgorithm.IV);
                     using (FileStream keyStream = File.OpenWrite("key.txt"))
                     {
                         using (StreamWriter writer = new StreamWriter(keyStream))
@@ -63,23 +65,26 @@ namespace Enigma
                     string outfile = args[4];
                     using (FileStream keyStream = File.OpenRead(args[3]))
                     {
-                        using (StreamReader keyReader= new StreamReader(keyStream))
+                        using (StreamReader keyReader = new StreamReader(keyStream))
                         {
-                            symmetricAlgorithm.Key = Convert.FromBase64String(keyReader.ReadLine());
-                            symmetricAlgorithm.IV = Convert.FromBase64String(keyReader.ReadLine());
+                            _symmetricAlgorithm.Key = Convert.FromBase64String(keyReader.ReadLine());
+                            _symmetricAlgorithm.IV = Convert.FromBase64String(keyReader.ReadLine());
                         }
                     }
 
-                    DecryptStringFromBytes(encryptedFile, outfile, symmetricAlgorithm.Key, symmetricAlgorithm.IV, symmetricAlgorithm);
+                    DecryptStringFromBytes(encryptedFile, outfile, _symmetricAlgorithm.Key, _symmetricAlgorithm.IV,
+                        _symmetricAlgorithm);
                 }
-
-
-
 
             }
             catch (Exception e)
             {
                 Console.WriteLine("Error: {0}", e.Message);
+            }
+            finally
+            {
+                if ( _symmetricAlgorithm != null )
+                _symmetricAlgorithm.Dispose();
             }
         }
         static void EncryptStringToBytes(string filename, string encryptedFile, byte[] Key,byte[] IV, SymmetricAlgorithm symmetricAlgorithm ) 
@@ -93,18 +98,19 @@ namespace Enigma
                 throw new ArgumentNullException("Key");
             byte[] encrypted;
 
-            ICryptoTransform encryptor = symmetricAlgorithm.CreateEncryptor(symmetricAlgorithm.Key, symmetricAlgorithm.IV);
-
-            using (FileStream msEncrypt = File.Open(encryptedFile, FileMode.OpenOrCreate))
+            using ( ICryptoTransform encryptor = symmetricAlgorithm.CreateEncryptor(symmetricAlgorithm.Key, symmetricAlgorithm.IV))
             {
-                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                using (FileStream msEncrypt = File.Open(encryptedFile, FileMode.OpenOrCreate))
                 {
-                    using (FileStream fs = File.OpenRead(filename))
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
                     {
-                        fs.CopyTo(csEncrypt);
+                        using (FileStream fs = File.OpenRead(filename))
+                        {
+                            fs.CopyTo(csEncrypt);
+
+                        }
 
                     }
-
                 }
             }
 
@@ -124,20 +130,22 @@ namespace Enigma
 
             symmetricAlgorithm.Key = Key;
             symmetricAlgorithm.IV = IV;
-
-            ICryptoTransform decryptor = symmetricAlgorithm.CreateDecryptor(symmetricAlgorithm.Key, symmetricAlgorithm.IV);
-
             File.Delete(outfile);
 
-            using (FileStream inputStream = File.Open( encryptedFile, FileMode.OpenOrCreate))
+
+            using ( ICryptoTransform decryptor = symmetricAlgorithm.CreateDecryptor(symmetricAlgorithm.Key, symmetricAlgorithm.IV))
             {
-                using (CryptoStream csDecrypt = new CryptoStream(inputStream, decryptor, CryptoStreamMode.Read))
+
+                using (FileStream inputStream = File.Open(encryptedFile, FileMode.OpenOrCreate))
                 {
-                    using (FileStream outFileStream = File.Open(outfile, FileMode.OpenOrCreate))
+                    using (CryptoStream csDecrypt = new CryptoStream(inputStream, decryptor, CryptoStreamMode.Read))
                     {
-                        csDecrypt.CopyTo(outFileStream);
+                        using (FileStream outFileStream = File.Open(outfile, FileMode.OpenOrCreate))
+                        {
+                            csDecrypt.CopyTo(outFileStream);
+                        }
                     }
-                }                
+                }
             }
             return ;    
         }
